aa1c6e1afe1c8e74cc194510b8a6ddb418db8735
Jerkbert <raballantyne@gmail.com>
2020-03-03 11:36:24 -0700

    added post route

--- back-end/db/seeds.rb
+++ back-end/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{name: "Rob", password: "testing"}])
+
+posts = Post.create([{content: "This is a post it is cool", likes: 0, user_id: 1}])
